dbb869b81ee5314dac055ba046e8893b8932bd65
Jasper Lauwers <jasper.lauwers@uantwerpen.be>
2016-11-06 13:44:19 +0100

    charge flip with MC subtraction

--- src/WeightCalc.cpp
+++ src/WeightCalc.cpp
@@ -94,7 +94,7 @@ void WeightCalc::initDYWeight(const EventReader& reader)
 //     maxVal = 3.9;
     
     // lepton eta weight
-    TFile* f = new TFile("DYWeight/DY_SSoverOS_2Dweight_2jet_tripleCharge.root","READ");
+    TFile* f = new TFile("DYWeight/DY_SSoverOS_2Dweight_2jet_tripleCharge_MCsubtr.root","READ");
     hDYshapeWeight = (TH2F*) f->Get("data_leptonabseta2D_weight"); //contains shape + normalization
 }
 
