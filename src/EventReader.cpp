7454e2ac620046b054f7bdf68b2e3dccb24a1b64
Jasper Lauwers <jasper.lauwers@uantwerpen.be>
2017-01-23 15:40:25 +0100

    fix loose lepton triple charge

--- src/EventReader.cpp
+++ src/EventReader.cpp
@@ -485,6 +485,7 @@ bool EventReader::fillNextEvent()
 //                 eventContainer.looseLeptons[iLepton].setClosestJetDr((*treeReader->std_vector_lepton_closejet_drlj)[iLepton]);
 //                 eventContainer.looseLeptons[iLepton].setClosestJetPartonFlavour((*treeReader->std_vector_lepton_closejet_PartonFlavour)[iLepton]);
 //                 eventContainer.looseLeptons[iLepton].setTrackIso((*treeReader->std_vector_lepton_trackIso)[iLepton]/(*treeReader->std_vector_lepton_pt)[iLepton]);
+                eventContainer.looseLeptons[iLepton].setTripleChargeAgreement((*treeReader->std_vector_electron_tripleChargeAgreement)[iLepton]);
                 
                 eventContainer.goodLeptons.push_back(iLepton);
                 eventContainer.leptons[iLepton].set((*treeReader->std_vector_lepton_pt)[iLepton],(*treeReader->std_vector_lepton_eta)[iLepton],(*treeReader->std_vector_lepton_phi)[iLepton],(*treeReader->std_vector_lepton_flavour)[iLepton], (*treeReader->std_vector_lepton_isTightLepton)[iLepton] );
