acc0810381eaf24c8c520855076da3ca4ebe7ae6
Jasper Lauwers <jasper.lauwers@uantwerpen.be>
2017-03-21 16:25:24 +0100

    update electron charge flip probability

--- src/WeightCalc.cpp
+++ src/WeightCalc.cpp
@@ -107,7 +107,7 @@ void WeightCalc::initDYWeight(const EventReader& reader)
 //     maxVal = 3.9;
     
     // lepton eta weight
-    TFile* f = new TFile("DYWeight/DY_SSoverOS_2Dweight_2jet_MCsubtr_Full2016.root","READ");
+    TFile* f = new TFile("DYWeight/DY_SSoverOS_2Dweight_2jet_MCsubtr_Feb2017.root","READ");
     hDYshapeWeight = (TH2D*) f->Get("data_leptonabseta2D_weight"); //contains shape + normalization
 }
 
